eb358b8ccc38242e565ecfbb8062940d8389ceff
Gautam Satish <gautams@hpe.com>
2021-05-03 13:35:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const https = require('https');
+const cp = require('child_process');
+const config = require("./config.json");
+
+let time = new Date();
+let formattedDate = '';
+
+const checkAppointments = function () {
+  console.log(`Running check at ${new Date()}`);
+  for (let i = 0; i < config.dayCount; i++ ) {
+    // API date format is DD-MM-YYYY
+    formattedDate = (time.getDate() + i) + "-" + (time.getMonth() + 1) + "-" + time.getFullYear();
+    console.log(`Looking for appointments on ${formattedDate}`);
+
+    https.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${config.districtId}&date=${formattedDate}`, (resp) => {
+      let data = '';
+      let slotAvailable = false;
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Check each session in each center for age match and availability.
+      resp.on('end', () => {
+        let centers = JSON.parse(data).centers;
+        centers.forEach(center => { 
+          center.sessions.forEach(session => {
+            if ((config.minAge >= session.min_age_limit) && (session.available_capacity > 0 )) {
+              slotAvailable = true;
+              console.log(`${session.available_capacity} slots available at ${center.name}[${center.center_id}], ${center.block_name} ${center.district_name} on ${session.date}.`);
+            }
+          });
+        });
+
+        // Sound the Beeper if slot is found.
+        if (config.soundNotify && slotAvailable )
+          cp.exec(`rundll32 user32.dll,MessageBeep`);
+        
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+  }
+  console.log(`Sleeping till next interval...`);
+}
+checkAppointments();
+setInterval(checkAppointments, config.checkInterval * 60000);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Check COVID-19 vaccination slots on Co-WIN
+
+Fetch the COVID-19 vaccination slots using the Co-WIN APIs 
+
+[Learn more about APIs](https://apisetu.gov.in/public/marketplace/api/cowin)
+
+## Requirements
+
+- Node and npm
+
+## Installation & Setup
+
+- Clone the repo: `git clone https://github.com/GautamSatish/restful_web_services.git`
+- Fill the required parameters in config.json
+- Start the script: `node index.js`
+- Use CTRL + C to terminate script
